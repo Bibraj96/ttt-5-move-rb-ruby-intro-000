da56522bed921661901a67dc1b4eb5d1e5b496ef
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-02-25 00:41:22 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,5 +9,13 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(user_input)
-  '0'.to_i
+  '1'.to_i
+  '2'.to_i
+  '3'.to_i
+  '4'.to_i
+  '5'.to_i
+  '6'.to_i
+  '7'.to_i
+  '8'.to_i
+  '9'.to_i
 end
